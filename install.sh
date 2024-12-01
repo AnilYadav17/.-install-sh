1aa9ea871f4a134a63045c1a9dc60dc465744a62
Anil Yadav <153111753+AnilYadav17@users.noreply.github.com>
2024-12-01 04:53:31 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Print a welcome message
+echo "Installing and running whatsapp_spam.sh..."
+
+# Download the main script
+curl -O https://raw.githubusercontent.com/AnilYadav17/whatspam.sh/main/whatsapp_spam.sh
+
+# Make the script executable
+chmod +x whatsapp_spam.sh
+
+# Run the script
+./whatsapp_spam.sh
+
+# Print a completion message
+echo "Installation and execution complete."
